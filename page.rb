0ed3052513267ab7f4e093b99fa77b4182e261f5
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-25 20:54:45 -0500

    page.rb: output page information

--- page.rb
+++ page.rb
@@ -15,8 +15,18 @@ class Page
     mechanize = Mechanize.new;
     self.class.const_set(:PAGE, mechanize.get("http://wikipedia.org/wiki/#{page_title}"))
   end
+
+  def tell_mw()
+    puts PAGE.search('//div[@class="mw-parser-output"]/p/a')
+  end
+
+  def analyze_page()
+    puts PAGE; puts PAGE.title; puts "tell"; self.tell_mw();
+  end
 end
 
 class InterfaceMachine
   puts page = Page.new(Input.new.get_input)
+  page.analyze_page ## put the if logic in!!
+  # tell_mw
 end
